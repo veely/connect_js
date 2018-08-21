838f76d1b1d3be7ef6c256422e65e51c485280e7
Vincent Ly <veely1@gmail.com>
2018-08-21 18:58:20 +0000

    cleans up code

--- knex_search.js
+++ knex_search.js
@@ -9,9 +9,6 @@ const knex = require('knex')({
   }
 });
 
-
-
-
 let userInput = process.argv[2];
 
 knex.select('first_name', 'last_name')
@@ -23,26 +20,3 @@ knex.select('first_name', 'last_name')
   console.log(rows);
   knex.destroy();
 });
-
-
-// const printData = function(data) {
-//   console.log("Found %i person(s) by the name '%s':", data.length, userInput);
-//   let itemNum = 1;
-//   for (let row of data) {
-//     console.log(itemNum + ": " + row.first_name + " " + row.last_name + ", born '" + row.birthdate + "'");
-//     itemNum++;
-//   }
-// };
-
-// client.connect((err) => {
-//   if (err) {
-//     return console.error("Connection Error", err);
-//   }
-//   client.query("SELECT * FROM famous_people WHERE first_name = $1::text OR last_name = $1::text;" , [userInput], (err, result) => {
-//     if (err) {
-//       return console.error("error running query", err);
-//     }
-//     printData(result.rows);
-//     client.end();
-//   });
-// });
